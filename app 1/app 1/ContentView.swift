0f5b624cef1eefc4b23cd73c2c75aa184b20f72c
Jamal-Ahmad-Alkandari <66321501+Jamal-Ahmad-Alkandari@users.noreply.github.com>
2022-07-30 12:55:37 +0300

    done 😎

---
+++ app 1/app 1/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  app 1
+//
+//  Created by Jamal Al-kandari on 30/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+                .opacity(Double.init(0.89))
+            
+            VStack{
+                
+                Text("Hello my name is Jamal")
+                    .padding()
+                    .colorInvert()
+                
+                Text("I'm 17 years old")
+                    .padding()
+                    .colorInvert()
+                   
+                Text("I like coding, cooking and deabating")
+                    .padding()
+                    .colorInvert()
+                    
+                HStack{
+                    
+                    Text("👨🏻‍🍳")
+                        Spacer()
+                    Text("🗣")
+                        Spacer()
+                    Text("👨🏻‍💻")
+                      
+                    
+                }
+                  
+            }
+           
+            
+        }
+        
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+
+    }
+}
---
+++ app 1/app 1/app_1App.swift
@@ -0,0 +1,17 @@
+//
+//  app_1App.swift
+//  app 1
+//
+//  Created by Jamal Al-kandari on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct app_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
